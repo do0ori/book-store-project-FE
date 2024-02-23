3f0c82593add3877acc36b8aa1b01f2024d35205
do0ori <fuzzydo0ori@gmail.com>
2024-02-24 03:01:10 +0900

    feat: Add theme context and global styles
    - themeContext.tsx for managing theme context
    - global.ts for global styles
    - theme.ts for theme configuration

---
+++ src/style/global.ts
@@ -0,0 +1,27 @@
+import "sanitize.css";
+import { createGlobalStyle } from "styled-components";
+import { ThemeName } from "./theme";
+
+interface Props {
+    themeName: ThemeName;
+}
+
+export const GlobalStyle = createGlobalStyle<Props>`
+    body {
+        padding: 0;
+        margin: 0;
+        background-color: ${
+            ({themeName}) => (themeName === "light") ? "white" : "black"
+        };
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    * {
+        color: ${
+          ({themeName}) => (themeName === "light") ? "black" : "white"
+        };
+    }
+`;
