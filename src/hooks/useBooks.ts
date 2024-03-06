653109aa77e7127b9acf999263275f68c44d9fdb
do0ori <fuzzydo0ori@gmail.com>
2024-03-06 23:17:09 +0900

    feat: Applied React Query(TanStack query) to the existing useBooks custom hook for managing book-related data fetching and caching.

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,35 +1,30 @@
 import { useLocation } from "react-router-dom";
-import { Book } from "../models/book.model";
-import { useEffect, useState } from "react";
-import { Pagination } from "../models/pagination.model";
 import { QUERYSTRING } from "../constants/querystrings";
 import { fetchBooks } from "../api/books.api";
 import { LIMIT } from "../constants/pagination";
+import { useQuery } from "@tanstack/react-query";
 
 export const useBooks = () => {
     const location = useLocation();
+    const params = new URLSearchParams(location.search);
 
-    const [books, setBooks] = useState<Book[]>([]);
-    const [pagination, setPagination] = useState<Pagination>({
-        currentPage: 1,
-        totalCount: 0
-    });
-    const [isEmpty, setIsEmpty] = useState(true);
-
-    useEffect(() => {
-        const params = new URLSearchParams(location.search);
-
-        fetchBooks({
+    const {
+        data: booksData,
+        isLoading: isBooksLoading
+    } = useQuery({
+        queryKey: ["books", location.search],
+        queryFn: () => fetchBooks({
             categoryId: params.get(QUERYSTRING.CATEGORY_ID) ? Number(params.get(QUERYSTRING.CATEGORY_ID)) : undefined,
             recent: params.get(QUERYSTRING.RECENT) ? true : undefined,
             limit: LIMIT,
             page: params.get(QUERYSTRING.PAGE) ? Number(params.get(QUERYSTRING.PAGE)) : 1,
-        }).then(({ books, pagination }) => {
-            setBooks(books);
-            setPagination(pagination);
-            setIsEmpty(books.length === 0);
-        })
-    }, [location.search]);
+        }),
+    });
 
-    return { books, pagination, isEmpty };
+    return {
+        books: booksData?.books,
+        pagination: booksData?.pagination,
+        isEmpty: booksData?.books.length === 0,
+        isBooksLoading
+    };
 };
