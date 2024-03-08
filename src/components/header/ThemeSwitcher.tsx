4acb1306e507fe9f09537110dfd14f543103b6e1
do0ori <fuzzydo0ori@gmail.com>
2024-03-08 17:02:07 +0900

    feat: Implement Dropdown component and integrate it into auth section in Header

--- src/components/header/ThemeSwitcher.tsx
+++ src/components/header/ThemeSwitcher.tsx
@@ -1,12 +1,28 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../context/themeContext";
+import { FaMoon, FaSun } from "react-icons/fa";
+import styled from "styled-components";
 
 function ThemeSwitcher() {
     const { themeName, toggleTheme } = useContext(ThemeContext)
 
     return (
-        <button onClick={toggleTheme}>{themeName}</button>
+        <ThemeSwitcherStyle onClick={toggleTheme}>{
+            themeName === "light"
+                ? <div className="theme-icon"><FaMoon /><span>Dark</span></div>
+                : <div className="theme-icon"><FaSun /><span>Light</span></div>
+        }</ThemeSwitcherStyle>
     );
 }
+
+const ThemeSwitcherStyle = styled.button`
+    .theme-icon {
+        svg {
+            width: 15px;
+            height: 15px;
+            fill: ${({ theme }) => theme.color.text};
+        }
+    }
+`;
 
 export default ThemeSwitcher;
