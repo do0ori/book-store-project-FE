4a8b62ee58420e04975fd24ea30c2d820ec5fbd2
do0ori <fuzzydo0ori@gmail.com>
2024-03-11 02:04:28 +0900

    feat: Update useMain hook to fetch new books and display them on the main page
    - Modified useMain.ts to also fetch new books and store them in the state.
    - Updated Home.tsx to include a section for displaying new books fetched from the useMain hook.

--- src/hooks/useMain.ts
+++ src/hooks/useMain.ts
@@ -1,16 +1,27 @@
+import { fetchBooks } from "@/api/books.api";
 import { fetchReviewAll } from "@/api/review.api";
+import { Book } from "@/models/book.model";
 import { BookReviewItem } from "@/models/review.model";
 import { useEffect, useState } from "react";
 
 export const useMain = () => {
     const [reviews, setReviews] = useState<BookReviewItem[]>([]);
+    const [newBooks, setNewBooks] = useState<Book[]>([]);
 
     useEffect(() => {
         fetchReviewAll()
             .then((reviews) => {
                 setReviews(reviews);
             });
+
+        fetchBooks({
+            recent: true,
+            limit: 4,
+            page: 1
+        }).then(({ books }) => {
+            setNewBooks(books);
+        });
     }, []);
 
-    return { reviews };
+    return { reviews, newBooks };
 };
